345c61566932fdb68e7cdb11a205ba51e562a280
envidi <phanm711996@gmail.com>
2024-05-08 17:57:02 +0700

    fix : thêm remove sidebar khi click outside

---
+++ index.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".menu");
+const body = document.querySelector("body");
+const openBtn = document.querySelector(".open");
+const closeBtn = document.querySelector(".close");
+body.addEventListener("click", function (e) {
+    const target = e.target
+    if(!menu.contains(target) && !openBtn.contains(target)){
+        menu.classList.remove('show')
+        return 
+    }
+
+});
+openBtn.addEventListener('click', function(){
+    menu.classList.add('show')
+})
+closeBtn.addEventListener('click', function(e){
+    menu.classList.remove('show')
+})
